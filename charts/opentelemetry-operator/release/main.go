056cc025420df3f8164f9354f61755b756dc2d76
Shibo Wang <shibw@amazon.com>
2021-07-22 23:01:52 -0700

    Rewrite image tags auto update

--- charts/opentelemetry-operator/release/main.go
+++ charts/opentelemetry-operator/release/main.go
@@ -7,22 +7,26 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"strings"
 )
 
 const (
-	operatorManifestURL = "https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml"
-	collectorCRDPath = "../crds/crd-opentelemetrycollector.yaml"
+	operatorManifestURL   = "https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml"
+	collectorCRDPath      = "../crds/crd-opentelemetrycollector.yaml"
+	valuesYAMLPath        = "../values.yaml"
+	chartYAMLPath         = "../Chart.yaml"
+	operatorRepoPath      = "repository: quay.io/opentelemetry/opentelemetry-operator"
+	kubeRBACProxyRepoPath = "repository: gcr.io/kubebuilder/kube-rbac-proxy"
+	appVersion            = "appVersion"
 )
 
 // K8sObject describes the values a Kubernetes object typically has.
 type K8sObject struct {
-	ApiVersion string `yaml:"apiVersion"`
-	Kind string `yaml:"kind"`
-	Metadata map[interface{}]interface{} `yaml:"metadata"`
-	Spec map[interface{}]interface{} `yaml:"spec"`
-	Status map[interface{}]interface{} `yaml:"status"`
+	ApiVersion string                      `yaml:"apiVersion"`
+	Kind       string                      `yaml:"kind"`
+	Metadata   map[interface{}]interface{} `yaml:"metadata"`
+	Spec       map[interface{}]interface{} `yaml:"spec"`
+	Status     map[interface{}]interface{} `yaml:"status"`
 }
 
 // The main keeps the OpenTelemetry Operator (OTEL Operator) Helm chart configurations up-to-date with the upstream automatically.
@@ -65,36 +69,48 @@ func main() {
 
 		case "Deployment":
 			// Retrieve the latest image repository and tag of the two container images.
-			managerImage := strings.Split(curObject.Spec["template"].(map[string]interface{})["spec"].
-				(map[string]interface{})["containers"].([]interface{})[0].(map[string]interface{})["image"].(string), ":")
-			kubeRBACProxyImage := strings.Split(curObject.Spec["template"].(map[string]interface{})["spec"].
-				(map[string]interface{})["containers"].([]interface{})[1].(map[string]interface{})["image"].(string), ":")
+			managerImage := strings.Split(curObject.Spec["template"].(map[string]interface{})["spec"].(map[string]interface{})["containers"].([]interface{})[0].(map[string]interface{})["image"].(string), ":")
+			kubeRBACProxyImage := strings.Split(curObject.Spec["template"].(map[string]interface{})["spec"].(map[string]interface{})["containers"].([]interface{})[1].(map[string]interface{})["image"].(string), ":")
 
 			// Replace the image tags in values.yaml and appVersion in Chart.yaml with the latest ones.
-			perlCommandStr1 := "s/    repository: quay.io\\/opentelemetry\\/opentelemetry-operator\\n    " +
-				"tag: \\\".*\\\"\\n  resources/    repository: quay.io\\/opentelemetry\\/opentelemetry-operator\\n    tag: \\\"" +
-				managerImage[1] + "\\\"\\n  resources/igs"
-			c1 := exec.Command("perl", "-0777", "-i", "-pe", perlCommandStr1, "../values.yaml")
-			err := c1.Run()
+			valuesFile, err := os.ReadFile(valuesYAMLPath)
 			if err != nil {
 				panic(err)
 			}
 
-			perlCommandStr2 := "s/    repository: gcr.io\\/kubebuilder\\/kube-rbac-proxy\\n    " +
-				"tag: \\\".*\\\"/    repository: gcr.io\\/kubebuilder\\/kube-rbac-proxy\\n    tag: \\\"" +
-				kubeRBACProxyImage[1] + "\\\"/igs"
-			c2 := exec.Command("perl", "-0777", "-i", "-pe", perlCommandStr2, "../values.yaml")
-			err = c2.Run()
+			valuesFileLines := strings.Split(string(valuesFile), "\n")
+			for i, line := range valuesFileLines {
+				if strings.Contains(line, operatorRepoPath) {
+					valuesFileLines[i+1] = "    tag: " + managerImage[1]
+				} else if strings.Contains(line, kubeRBACProxyRepoPath) {
+					valuesFileLines[i+1] = "    tag: " + kubeRBACProxyImage[1]
+				}
+			}
+
+			valuesFileOutput := strings.Join(valuesFileLines, "\n")
+			err = os.WriteFile(valuesYAMLPath, []byte(valuesFileOutput), 0644)
 			if err != nil {
 				panic(err)
 			}
 
-			perlCommandStr3 := "s/appVersion: \\\".*\\\"/appVersion: \\\"" + managerImage[1][1:] + "\\\"/igs"
-			c3 := exec.Command("perl", "-0777", "-i", "-pe", perlCommandStr3, "../Chart.yaml")
-			err = c3.Run()
+			chartFile, err := os.ReadFile(chartYAMLPath)
 			if err != nil {
 				panic(err)
 			}
+
+			chartFileLines := strings.Split(string(chartFile), "\n")
+			for i, line := range chartFileLines {
+				if strings.Contains(line, appVersion) {
+					chartFileLines[i] = appVersion + ": " + managerImage[1][1:]
+				}
+			}
+
+			chartFileOutput := strings.Join(chartFileLines, "\n")
+			err = os.WriteFile(chartYAMLPath, []byte(chartFileOutput), 0644)
+			if err != nil {
+				panic(err)
+			}
+
 			log.Println("The values.yaml and Chart.yaml update finished. They are up-to-date now.")
 
 		default:
